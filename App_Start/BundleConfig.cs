205ec00d4e9697d44c980d46a43a2291f9d2bb5e
Miguel Angel Segura Sandoval <miguel.sesandoval@gmail.com>
2021-05-24 22:14:36 -0500

    Add project files.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,52 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SeSandoval
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            /*
+             * Scripts
+             */
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/Lib/mdc").Include(
+            //            "~/Lib/MDC/material-components-web.js"));
+
+            bundles.Add(new ScriptBundle("~/Lib/vuejs").Include(
+                        "~/Lib/Vuejs/vue.js"));
+
+            bundles.Add(new ScriptBundle("~/Lib/vuetifyjs").Include(
+                        "~/Lib/Vuetify/vuetify.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/shared").Include(
+                        "~/Scripts/Shared/main.js"));
+
+            /*
+             * Style
+             */
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/SeSandovalStyle.css"));
+
+            //bundles.Add(new StyleBundle("~/Lib/mdc").Include(
+            //          "~/Lib/MDC/material-components-web.css"));
+
+            bundles.Add(new StyleBundle("~/Lib/vuetifycss").Include(
+                      "~/Lib/Vuetify/vuetify.css"));
+        }
+    }
+}
---
+++ Scripts/Shared/main.js
@@ -0,0 +1,58 @@
+﻿const app = new Vue({
+    el: '#Layout',
+    vuetify: new Vuetify(),
+    data() {
+        return {
+            viewTabs: null,
+            LanguageEn: true,
+            Contact: [
+                english = 'Contact me',
+                spanish = 'Contáctame',
+                tooltipEn = 'If you want to get in touch, click here.',
+                tooltipEs = 'Si quieres ponerte en contacto, da click aquí.',
+                icon = 'contact_mail'
+            ],
+            BookList: [
+                english = 'Favorite book list',
+                spanish = 'Lista de libros favoritos',
+                tooltipEn = "Some of the best books I've read through my life.",
+                tooltipEs = 'Algunos de los mejores libros que he leído en el transcurso de mi vida.',
+                icon = 'menu_book'
+            ],
+            FAQ: [
+                english = 'Frequently Asked Questions',
+                spanish = 'Preguntas frecuentes',
+                tooltipEn = 'Questions people asks me oftenly.',
+                tooltipEs = 'Preguntas frecuentes.',
+                icon = 'help_outline'
+            ],
+            Language: [
+                english = 'Language',
+                spanish = 'Lenguaje',
+                tooltipEn = 'Change language to english.',
+                tooltipEs = 'Cambiar idioma a español (Actualmente no disponible).',
+                options =
+                [
+                    [
+                        english = 'English',
+                        spanish = 'Inglés',
+                    ],
+                    [
+                        english = 'Spanish',
+                        spanish = 'Español',
+                    ]
+                ],
+                icon = 'language'
+            ],
+
+        }
+    },
+    methods: {
+        LanguageToEn: function (event) {
+            this.LanguageEn = true;
+        },
+        LanguageToEs: function (event) {
+            this.LanguageEn = false;
+        }
+    }
+})
